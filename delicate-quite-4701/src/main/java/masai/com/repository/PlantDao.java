f23ae4b578c2a725eab5f073996955634185fd10
Shanu Kumrawat <101422764+shanukumrawat@users.noreply.github.com>
2022-11-11 14:57:16 +0530

    Update PlantDao.java

--- delicate-quite-4701/src/main/java/masai/com/repository/PlantDao.java
+++ delicate-quite-4701/src/main/java/masai/com/repository/PlantDao.java
@@ -1,5 +1,22 @@
 package masai.com.repository;
 
-public class PlantDao {
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import masai.com.model.Plant;
+
+@Repository
+public interface PlantDao extends JpaRepository<Plant, Integer>{
+	
+	@Query("select p from Plant p where p.commonName = ?1")
+	public Plant viewPlantBycommonName(String commonName);
+	
+	@Query("select p from Plant p where p.typeOfPlant = ?1")
+     public List<Plant> viewPlantBytypeOfPlant(String typeOfPlant);
+	
 
 }
