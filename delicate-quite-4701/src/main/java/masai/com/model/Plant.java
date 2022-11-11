728d95d3d4c6235c868dbc5fd68d027b6cc6d82c
Shanu Kumrawat <101422764+shanukumrawat@users.noreply.github.com>
2022-11-11 14:55:28 +0530

    Update Plant.java

--- delicate-quite-4701/src/main/java/masai/com/model/Plant.java
+++ delicate-quite-4701/src/main/java/masai/com/model/Plant.java
@@ -1,5 +1,55 @@
 package masai.com.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Plant {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer plantId;
+	
+	@NotNull
+	private Integer plantHeight;
+	
+	@NotNull
+	private String plantSpread;
+	
+	private String commonName;
+	
+	private String bloomTime;
+	
+	@NotNull
+	private String medicinalOrCulinaryUse;
+	
+	private String difficultyLevel;
+	
+	private String temparature;
+	
+	@NotNull
+	private String typeOfPlant;
+	
+	private String plantDescription;
+	
+	@NotNull
+	private Integer plantStock;
+	
+	@NotNull
+	private double plantCost;
+	
+
 }
+
