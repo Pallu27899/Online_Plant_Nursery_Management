b22d2a816876c728d451d87a055d46eb391231d6
Pallavi Bobale <101388809+Pallu27899@users.noreply.github.com>
2022-11-11 22:00:38 +0530

    Update SeedServices.java

--- delicate-quite-4701/src/main/java/masai/com/service/SeedServices.java
+++ delicate-quite-4701/src/main/java/masai/com/service/SeedServices.java
@@ -1,5 +1,23 @@
 package masai.com.service;
+import java.util.List;
 
-public class SeedServices {
+import masai.com.exception.SeedException;
+import masai.com.model.Seed;
 
+public interface SeedServices {
+
+    public Seed addSeed(Seed seed)throws SeedException;
+
+    public Seed updateSeed(Seed seed)throws SeedException;
+
+    public Seed deleteSeed(Seed seed)throws SeedException;
+
+    public List<Seed> getAllSeeds()throws SeedException;
+
+    public Seed getSeedById(Integer seedId) throws SeedException;
+
+    public List<Seed> getSeedBycommonName(String commonName) throws SeedException;
+
+    public List<Seed> getseedByTypeOfSeed(String typeOfSeeds) throws SeedException;	
+	
 }
