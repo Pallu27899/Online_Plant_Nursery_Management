314ee88e20ff2e18bfffa4d6af4ac333f7fc8a3a
Pallavi Bobale <101388809+Pallu27899@users.noreply.github.com>
2022-11-12 00:07:34 +0530

    Update SeedDao.java

--- delicate-quite-4701/src/main/java/masai/com/repository/SeedDao.java
+++ delicate-quite-4701/src/main/java/masai/com/repository/SeedDao.java
@@ -1,5 +1,22 @@
 package masai.com.repository;
 
-public class SeedDao {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import masai.com.model.Seed;
+
+@Repository
+public interface SeedDao extends JpaRepository<Seed, Integer>{
+
+	
+	@Query("select s from Seed s where s.commonName = ?1")
+	public List<Seed> getSeedBycommonName(String commonName);
+	
+	@Query("select s from Seed s where s.typeOfSeeds = ?1")
+     public List<Seed> getSeedBytypeOfSeeds(String typeOfSeeds);
+
+	 
 }
