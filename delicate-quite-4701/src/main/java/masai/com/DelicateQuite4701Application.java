616bca5c3c4aaeea863224d89d1a5552e0cb4485
Shanu Kumrawat <101422764+shanukumrawat@users.noreply.github.com>
2022-11-12 15:30:28 +0530

    Update DelicateQuite4701Application.java

--- delicate-quite-4701/src/main/java/masai/com/DelicateQuite4701Application.java
+++ delicate-quite-4701/src/main/java/masai/com/DelicateQuite4701Application.java
@@ -3,6 +3,8 @@ package masai.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 public class DelicateQuite4701Application {
 
