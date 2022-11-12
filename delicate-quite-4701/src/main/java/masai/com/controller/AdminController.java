2e63c54b997b240c0114800cedc2bcfd1d9a6d0e
sameerbhavar <sameerbhavar143@gmail.com>
2022-11-12 11:12:13 +0530

    admin controller creation

--- delicate-quite-4701/src/main/java/masai/com/controller/AdminController.java
+++ delicate-quite-4701/src/main/java/masai/com/controller/AdminController.java
@@ -1,5 +1,30 @@
 package masai.com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import masai.com.service.OrderService;
+import masai.com.service.PlantServices;
+import masai.com.service.PlanterService;
+import masai.com.service.SeedServices;
+import masai.com.service.SignUpService;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	private SignUpService signupservice;
+	@Autowired
+	private SeedServices seedservice;
+	@Autowired
+	private OrderService orderservice;
+	
+	
+	@Autowired
+	private PlantServices plantServices;
+	
+	@Autowired
+	private PlanterService planterService;
 }
