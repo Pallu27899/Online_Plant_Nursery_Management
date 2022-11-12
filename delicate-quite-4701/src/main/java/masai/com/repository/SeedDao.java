e1aacd8badbd433746c06ec29eb1e0e39d042f0d
sameerbhavar <sameerbhavar143@gmail.com>
2022-11-12 11:18:42 +0530

    admin

--- delicate-quite-4701/src/main/java/masai/com/repository/SeedDao.java
+++ delicate-quite-4701/src/main/java/masai/com/repository/SeedDao.java
@@ -1,6 +1,8 @@
 package masai.com.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
